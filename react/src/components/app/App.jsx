388414adda2139aa96b65058523dc97cd1ba98f9
Petros Anagnou <petros.anagnoy@gmail.com>
2025-09-06 17:31:39 +0300

    replace crypto.randomuuid

--- react/src/components/app/App.jsx
+++ react/src/components/app/App.jsx
@@ -5,10 +5,17 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import About from "../about/about.jsx";
 
 
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0; // random 0-15
+        const v = c === 'x' ? r : (r & 0x3 | 0x8); // version 4 & variant
+        return v.toString(16);
+    });
+}
 
 function App() {
 
-    const doodleId = crypto.randomUUID();
+    const doodleId = generateUUID()
 
     return <>
         <h1 id="title">
