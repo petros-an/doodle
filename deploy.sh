4e477de8891766f3d663d5ad01717fae66d56494
Petros Anagnou <p.anagnoy@iwoca.co.uk>
2026-02-01 13:09:17 +0000

    Deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd react
-docker build -t petrosan/doodle_frontend:latest --build-arg VITE_BACKEND_URL=https://172.236.13.62:8000 --no-cache .
+docker build -t petrosan/doodle_frontend:latest --build-arg VITE_BACKEND_URL=https://petros-an.com:8000 --no-cache .
 
 cd ..
 
@@ -11,5 +11,5 @@ docker push petrosan/doodle_backend:latest
 
 cd ..
 
-ssh petros@"$deploy_host" "cd doodle; docker compose up -d --pull always"
+ssh petros@"$deploy_host" "cd doodle; docker compose pull backend frontend; docker compose up -d"
 
